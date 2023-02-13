df9ed70559c57c8705523b549dbc3668f79bd31f
Aluerie <aluerie@gmail.com>
2023-02-13 19:54:37 +0300

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -50,5 +50,5 @@ with open("./src/alu.yaml", 'r') as yaml_in:
 data.pop('alu')
 
 # final dump
-with open('./themes/alu_dark_pycharm-high-contrast.json', 'w', encoding='utf-8') as f:
+with open('./themes/alu_dark_pycharm-high-contrast-color-theme.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
